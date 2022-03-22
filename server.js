de81b2e9458d73a10802204dfb0214710fd2d6c0
compuwill <wpvorngsam@hotmail.com>
2022-03-21 20:52:22 -0500

    Fixed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -52,6 +53,6 @@ function filterByQuery(query, animalsArray) {
   });
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
